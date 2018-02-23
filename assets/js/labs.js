0653fb686e88bb6708142aefde65566e26814ff5
Samir Wadhwania <samirw@mit.edu>
2018-02-22 22:02:13 -0500

    Changed to local access rather than remote access

--- assets/js/labs.js
+++ assets/js/labs.js
@@ -2,9 +2,7 @@ function run(post, target) {
     var converter = new showdown.Converter();
 
     var oRequest = new XMLHttpRequest(),
-        sURL = "https://"
-        + self.location.hostname
-        + post,
+        sURL = post,
         target = document.getElementById(target),
         allText = "";
 
@@ -22,9 +20,9 @@ function run(post, target) {
     oRequest.send(null);
 }
 
-run("/assets/posts/about.md", "about")
-run("/assets/posts/lab3.md", "lab3")
-run("/assets/posts/lab4.md", "lab4")
-run("/assets/posts/lab6.md", "lab6")
-run("/assets/posts/lab7.md", "lab7")
-run("/assets/posts/lab8.md", "lab8")
+run("assets/posts/about.md", "about")
+run("assets/posts/lab3.md", "lab3")
+run("assets/posts/lab4.md", "lab4")
+run("assets/posts/lab6.md", "lab6")
+run("assets/posts/lab7.md", "lab7")
+run("assets/posts/lab8.md", "lab8")
